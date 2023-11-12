785abb88bfa61e77528ca1219e39b5fae05f0f3f
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-11 19:40:17 -0500

    aob-39: Added simple smoke test
    **Problem:**
    
    **Fix:**
    Added simple smoke test to check that all necessary components are
    loaded in the context
    
    **Testing:**

---
+++ src/test/java/project11/amazinbookstore/SmokeTest.java
@@ -0,0 +1,43 @@
+package project11.amazinbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import project11.amazinbookstore.controller.UserRegistrationController;
+import project11.amazinbookstore.controller.rest.BookStoreRESTController;
+import project11.amazinbookstore.repository.BookRepository;
+import project11.amazinbookstore.repository.UserRepository;
+import project11.amazinbookstore.services.BookService;
+import project11.amazinbookstore.services.UserService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Simple smoke test for ensuring that all necessary components are
+ * created in the context.
+ */
+@SpringBootTest
+public class SmokeTest {
+    @Autowired private BookStoreRESTController bookStoreRESTController;
+    @Autowired private UserRegistrationController userRegistrationController;
+    @Autowired private BookService bookService;
+    @Autowired private UserService userService;
+    @Autowired private BookRepository bookRepository;
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    void contextLoads() throws Exception {
+        Object[] contextComponents = {
+                bookStoreRESTController,
+                userRegistrationController,
+                bookService,
+                userService,
+                bookRepository,
+                userRepository
+        };
+
+        for (Object o : contextComponents) {
+            assertThat(o).isNotNull();
+        }
+    }
+}
