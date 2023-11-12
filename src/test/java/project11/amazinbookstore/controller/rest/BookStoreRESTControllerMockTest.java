2f0b32ec747f7c3bf7f93778da6d820af2c89c1c
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-11 21:50:25 -0500

    aob-39: Current progress with BookStoreRESTControllerMockTest
    **Problem:**
    Not working. Testing that role-based access is working is kinda wonky?
    
    **Fix:**
    Unfinished progress
    
    **Testing:**

--- src/test/java/project11/amazinbookstore/controller/rest/BookStoreRESTControllerMockTest.java
+++ src/test/java/project11/amazinbookstore/controller/rest/BookStoreRESTControllerMockTest.java
@@ -1,5 +1,7 @@
 package project11.amazinbookstore.controller.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import project11.amazinbookstore.model.Book;
 import project11.amazinbookstore.services.BookService;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,7 +25,7 @@ import java.util.ArrayList;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(BookStoreRESTController.class)
-@WithMockUser
+@WithMockUser(roles = {"USER"})
 class BookStoreRESTControllerMockTest {
 
     @Autowired
@@ -30,12 +34,57 @@ class BookStoreRESTControllerMockTest {
     @MockBean
     private BookService bookService;
 
+    private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    /**
+     * Validates
+     * @throws Exception
+     */
     @Test
     void testGetAllBooksEmpty() throws Exception {
         when(bookService.getAllAvailableBooks()).thenReturn(new ArrayList<>());
-        mockMvc.perform(get("/api/book/all").with(csrf()))
+        mockMvc.perform(get("/api/book/all"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("[]"));
+    }
+
+    @Test
+    @WithMockUser(roles = {"USER"})
+    void testAddBookAsUser() throws Exception {
+        Book newBook = new Book("title", "author", "genres", "picture", 5);
+        newBook.setId(1L);
+
+        String requestJson = ow.writeValueAsString(newBook);
+
+        when(bookService.addBook(newBook)).thenReturn(newBook);
+        mockMvc.perform(post("/api/book/admin/add")
+                        .content(requestJson)
+                        .contentType("application/json")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    void testAddBookAsAdmin() throws Exception {
+        Book newBook = new Book("title", "author", "genres", "picture", 5);
+        newBook.setId(1L);
+
+        String requestJson = ow.writeValueAsString(newBook);
+
+        when(bookService.addBook(newBook)).thenReturn(newBook);
+        mockMvc.perform(post("/api/book/admin/add")
+                        .content(requestJson)
+                        .with(csrf())
+                        .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(requestJson));
     }
 }
