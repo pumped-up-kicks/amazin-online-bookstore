776a5b9d452828b329173f895eb8bd2fa4de6220
Zakaria Ismail <48291075+1112zakaria@users.noreply.github.com>
2023-11-12 21:32:50 -0500

    aob-39: Add unit tests for Book CRUD functions (#41)
    **Problem:**
    closes #39 
    Unit tests needed for the above changes.
    
    **Fix:**
    Added tests for BookStoreRESTController.java
    Added tests for BookService.java
    Added smoke test to check that all required components are instantiated in the context
    
    **Testing:**
    All tests pass.
    
    **Dependencies:**
    
    **Observability:**
    
    ![image](https://github.com/pumped-up-kicks/amazin-online-bookstore/assets/48291075/16d34d2a-1a10-4d24-be09-b69ec7140870)
    
    All tests pass.
    
    **Notes:**
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/project11/amazinbookstore/model/Book.java
+++ src/main/java/project11/amazinbookstore/model/Book.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "book")
@@ -92,5 +93,18 @@ public class Book {
 
     public void setInventoryQuantity(int quantity) {
         this.inventoryQuantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(genres, book.genres) && Objects.equals(picture, book.picture);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, genres, picture);
     }
 }
