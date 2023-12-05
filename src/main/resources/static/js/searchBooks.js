ee2a44e79c63048d0d487ed44f03121c98f0efcb
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-05 02:57:19 -0500

    aob-101: basic on-type event js handling
    **Problem:**
    
    **Fix:**
    also changed class to id, its easier
    
    **Testing:**

--- src/main/resources/static/js/searchBooks.js
+++ src/main/resources/static/js/searchBooks.js
@@ -2,8 +2,18 @@
 $(document).ready(function () {
     console.log('Document is ready.');
 
-    $('.search-bar').on('input', function (e) {
+    $('#search-bar').on('input', function (e) {
         console.log('input changed');
+        let inputContent = $('#search-bar').val();
+        let url = '/api/book?title=' + inputContent;
+        console.log(inputContent);
+        // fetch data
+
+        // write content onto the DOM
+        $.get(url, function (data, status) {
+            console.log(data);
+            console.log(status);
+        });
     });
 })
 
