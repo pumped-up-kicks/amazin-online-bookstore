3f592fb7ff0f2aa8aef1e978425abf90f2c42615
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-06 13:59:38 -0500

    aob-101: cleanup
    **Problem:**
    
    **Fix:**
    
    **Testing:**

--- src/main/resources/static/js/searchBooks.js
+++ src/main/resources/static/js/searchBooks.js
@@ -5,17 +5,8 @@ $(document).ready(function () {
     $('#search-bar').on('input', function (e) {
         console.log('input changed');
         let inputContent = $(this).val();
-        // FIXME: input content with spaces are not being included in URL
         let url = '/api/book/search?query=' + encodeURIComponent(inputContent);
         console.log(inputContent);
-        // fetch data
-
-        // write content onto the DOM
-        $.get(url, function (data, status) {
-            console.log(data);
-            console.log(status);
-            //var classes = $(data).filter("container").attr("")
-        });
 
         $("#search-results-block").load(url);
     });
